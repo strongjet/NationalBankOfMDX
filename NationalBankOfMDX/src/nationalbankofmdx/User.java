ee60e84734bac2f7e65c118bbfb46a75e2b57215
Radwane <rad-wane@hotmail.co.uk>
2018-01-10 13:43:29 +0000

    variable data type alignment

---
+++ NationalBankOfMDX/src/nationalbankofmdx/User.java
@@ -0,0 +1,56 @@
+package nationalbankofmdx;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author Radwane
+ */
+public class User {
+
+    private final String name;
+    private final String surname;
+    private final List<Double> transactionList;
+    private final BankAccount bankAccount;    
+
+    public User(String n, String s, List<Double> tL, BankAccount bA) {
+        this.name = n;
+        this.surname = s;
+        this.transactionList = tL;
+        this.bankAccount = bA;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public List<Double> gettransactionList() {
+        return transactionList;
+    }    
+    
+     public void ActionTransactionList() {
+        System.out.println("Account " + bankAccount.getAccountNo() + ": current balance is: " + bankAccount.getAccountBalance());
+        for (Double transaction : transactionList) {
+
+            if (transaction < 0) {
+                System.out.println(name + " " + surname + " is trying to withdraw £" + transaction + " from account number: " + bankAccount.getAccountNo());
+                bankAccount.withdraw(transaction);
+                System.out.println("New balance on account " + bankAccount.getAccountNo() + " is £" + bankAccount.getAccountBalance());
+            } else {
+
+                System.out.println(name + " " + surname + " is depositing £" + transaction + " from account number: " + bankAccount.getAccountNo());
+                bankAccount.deposit(transaction);
+                System.out.println("New balance on account " + bankAccount.getAccountNo() + " is £" + bankAccount.getAccountBalance());
+            }
+        }
+    }
+
+    public double userBalance() {
+        return bankAccount.getAccountBalance();
+    }     
+}
