95d9451b64f001c2eaa7c6be92786fce068c4fab
Radwane <rad-wane@hotmail.co.uk>
2018-01-10 11:29:15 +0000

    Transaction list to be executed during run time, also a userBalance getter class is made.

--- src/nationalbankofmdx/User.java
+++ src/nationalbankofmdx/User.java
@@ -32,7 +32,24 @@ public class User {
         return transactionList;
     }    
     
-    
-    
-    
+     public void ActionTransactionList() {
+        System.out.println("Account " + bankAccount.getAccountNo() + ": current balance is: " + bankAccount.getAccountBalance());
+        for (Double transaction : transactionList) {
+
+            if (transaction < 0) {
+                System.out.println(name + " " + surname + " is trying to withdraw £" + transaction + " from account number: " + bankAccount.getAccountNo());
+                bankAccount.withdraw(transaction);
+                System.out.println("New balance on account " + bankAccount.getAccountNo() + " is £" + bankAccount.getAccountBalance());
+            } else {
+
+                System.out.println(name + " " + surname + " is depositing £" + transaction + " from account number: " + bankAccount.getAccountNo());
+                bankAccount.deposit(transaction);
+                System.out.println("New balance on account " + bankAccount.getAccountNo() + " is £" + bankAccount.getAccountBalance());
+            }
+        }
+    }
+
+    public Double userBalance() {
+        return bankAccount.getAccountBalance();
+    }     
 }
